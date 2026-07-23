81b37b35dbe495fb3ce1a478f117e27db6241595
YOUR_OLD_GITHUB_USERNAME <meerasoundharya03@gmail.com>
2026-07-23 21:23:28 +0530

    chore: enforce strict port 5173 for consistent localhost url

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,8 @@ export default defineConfig({
     },
   },
   server: {
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/geo-api': {
         target: 'https://nominatim.openstreetmap.org',
